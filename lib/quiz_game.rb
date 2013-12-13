bef54c991040d5911a2c5246b61b9d55ed5289d2
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-12 19:25:05 -0500

    Add questions and views

--- lib/quiz_game.rb
+++ lib/quiz_game.rb
@@ -3,6 +3,7 @@ class QuizGame
 	end
 
 	def play
-		["Nice! You're a Pokemon!","I would love to learn more about you...just start talking I'll listen","Sorry that's not correct, but you're still pretty neat to me","Now THATS what I call joke!"].sample
+		["Nice! You're a Pokemon!","I would love to learn more about you...just start talking I'll listen","Sorry that's not correct, but you're still pretty neat to me","Now THATS what I call joke!","YAY! You're my best friend!"].sample
 	end
+
 end
